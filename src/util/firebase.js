7cdc41d42149853ddc5ea3c51566e676990c0883
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-07-24 00:49:19 +0530

    final commit

--- src/util/firebase.js
+++ src/util/firebase.js
@@ -1,16 +1,10 @@
-var admin = require("firebase-admin");
-
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 const decoded = Buffer.from(process.env.FIREBASE_ADMIN_BASE64, 'base64').toString('utf-8');
 const serviceAccount = JSON.parse(decoded);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-});
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
