63619a5e041a25dc288ff4fb8f543e30bb17d62c
Mayank Kumar Sharma <mayankfhacker@gmail.com>
2025-07-23 23:21:37 +0530

    final commit

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const firebaseRoute = require('./src/routes/FirebaseRoute');
 const cors = require('cors');
+require("dotenv").config();
 
 const app = express(); // ✅ Initialize before using
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware setup
 app.use(bodyParser.json());
